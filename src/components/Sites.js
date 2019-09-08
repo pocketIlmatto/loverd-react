33418af838babfd32366ec6211fc3efae6356e9e
Pamela Kinnaird <pocket.ilmatto@gmail.com>
2019-09-07 21:35:36 -0700

    Create a SparkLineChart with site data from gql

--- src/components/Sites.js
+++ src/components/Sites.js
@@ -1,30 +1,67 @@
 import React, { Component } from 'react';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
+import SparkLineChart from './SparkLineChart';
 
-const SITES_QUERY = gql`
-  query {
+const GET_SITES = gql`
+  query Sites($parameterType: String!) {
     sites {
       id
       name
+      lat
+      lon
+      filteredSiteData(parameterType: $parameterType) {
+        parameterType
+        value
+        unit
+        timestamp
+      }
     }
   }
 `;
 
 class Sites extends Component {
+  state = {
+    parameterType: "current_temp"
+  };
+
+  chartData(siteData) {
+    return siteData.map((d) => {
+      return d.value
+    })
+  }
+
+  chartLabels(siteData) {
+    return siteData.map((d) => {
+      return ''
+    })
+  }
+
   render() {
+    const { parameterType } = this.state;
+
     return (
-      <Query query={SITES_QUERY}>
+      <Query query={GET_SITES} variables={{ parameterType }}>
         {({ loading, error, data }) => {
           if (loading) return <div>Fetching..</div>
           if (error) return <div>Error!</div>
           return (
-            <div className="flex flex-wrap">
+            <div className="flex flex-wrap sites-list">
               {data.sites.map((site) => {
-                return <div key={site.id} className="w-full site-entry"
-                        onClick={this.props.selectSite.bind(this, site)}>>
-                    <div className="font-bold text-xl mb-2">{site.name}</div>
-                </div>
+                const chartData = site.filteredSiteData.map(d => d.value);
+                return <div key={site.id}
+                        className="w-full site-entry-row-container"
+                        onClick={this.props.selectSite.bind(this, site)}>
+                        <div className="flex site-entry-row">
+                          <div className="font-bold text-xl mb-2 w-1/2 site-name">
+                            {site.name}
+                          </div>
+                          <div className="w-1/2 mb-1 site-chart" key={'cjs-' + site.id}>
+                            <SparkLineChart data={chartData} color={'#70cad1'}/>
+                          </div>
+                        </div>
+                        <hr/>
+                      </div>
               })}
             </div>
           )
